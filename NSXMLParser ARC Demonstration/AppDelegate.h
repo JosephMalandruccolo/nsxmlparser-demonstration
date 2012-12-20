7e14648e3a6d216b881b39e58fcd438bcbdda2b0
Robert Ryan <rryan@Robert-Ryans-iMac.local>
2012-12-20 00:41:34 -0500

    Initial Commit

---
+++ NSXMLParser ARC Demonstration/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSXMLParser ARC Demonstration
+//
+//  Created by Robert Ryan on 12/20/12.
+//  Copyright (c) 2012 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSXMLParser ARC Demonstration/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NSXMLParser ARC Demonstration
+//
+//  Created by Robert Ryan on 12/20/12.
+//  Copyright (c) 2012 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
